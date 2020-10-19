4b0e7842fd786ac8f609fb933878fc1f7cc45943
alessandro <alessandro.massarella@gmail.com>
2020-10-19 16:16:09 +0200

    Password Generator

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var nomeUser = prompt ('Ciao, inserisci il tuo nome?');
+
+var cognomeUser = prompt ('E ora il tuo cognome');
+
+var colore = prompt ('Infine inserisci il tuo colore preferito');
+
+document.getElementById ('password').innerHTML = 'La password generata è: ' + nomeUser + cognomeUser + colore + '19'
